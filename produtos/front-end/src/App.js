c363cc00e9c1f5b4760cfa796fb03ba872fdd19c
Tainá Barbosa <160445609+Nabarbosa@users.noreply.github.com>
2025-10-20 11:15:27 -0300

    Add files via upload

---
+++ produtos/front-end/src/App.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import Formulario from './Formulario';
+import Tabela from './Tabela';
+
+function App() {
+  const[btnCadastrar, setBtnCadastrar] = useState(true);
+  return (
+    <div>
+      <Formulario botao = {btnCadastrar}/>
+      <Tabela/>
+    </div>
+  );
+}
+
+export default App;
---
+++ produtos/src/main/java/br/com/api/produtos/controller/ProdutoController.java
@@ -0,0 +1,24 @@
+package br.com.api.produtos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.api.produtos.model.ProdutoModel;
+import br.com.api.produtos.service.ProdutoService;
+
+@RestController
+public class ProdutoController {
+    @Autowired
+    private ProdutoService ps;
+
+    @GetMapping("/listar")
+    public Iterable<ProdutoModel> listar(){
+        return ps.listar();
+    }
+    
+    @GetMapping("")
+    public String rota() {
+        return "api de produtos funcionando!";
+    }
+}
